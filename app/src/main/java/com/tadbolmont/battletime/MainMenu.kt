812774c1ea80971e075dd3d327ed7294e9bd6109
Chris Poppell <clpoppell@valdosta.edu>
2019-06-14 23:26:11 -0400

    Initial commit

---
+++ app/src/main/java/com/tadbolmont/battletime/MainMenu.kt
@@ -0,0 +1,12 @@
+package com.tadbolmont.battletime
+
+import android.app.Activity
+import android.os.Bundle
+
+class MainMenu : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_menu)
+    }
+}
